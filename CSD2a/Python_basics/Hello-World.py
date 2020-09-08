655816ec6d987ce4dc7f658a6e598ba412baa716
harm <65293494+harmneidig@users.noreply.github.com>
2020-09-08 11:57:07 +0200

    removed python_basics folder

--- CSD2a/Python_basics/Hello-World.py
+++
@@ -1,7 +0,0 @@
-username = input("Wat is je naam?")
-wachtwoord = float (input("Wat is pi met 2 decimalen?"))
-
-if wachtwoord == 3.14:
-        print("Goed gedaan, " + username + ", ga zo door!")
-else:
-        print("FOUT")
