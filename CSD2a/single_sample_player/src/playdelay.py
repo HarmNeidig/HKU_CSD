5bab4d1ab570595ad71658f88a144b4e64bcf3ac
harm <65293494+harmneidig@users.noreply.github.com>
2020-09-15 15:32:00 +0200

    Add quit() function and fixed some bugs

--- CSD2a/single_sample_player/src/playdelay.py
+++ CSD2a/single_sample_player/src/playdelay.py
@@ -10,19 +10,20 @@ bpm = in_list.pop(0)
 print(in_list)
 print(bpm)
 
-
 while t==0:
     Antwoord = input("do you want to hear your creation? y / n")
     if Antwoord == "y":
         for f in in_list:
             play_obj = kick.play()
-            time.sleep(float(f)*(60/(int (bpm))))
+            time.sleep(float(f)*(float(60/(int(bpm)))))
     if Antwoord == "n":
         Antwoord2 = input("do you want to change your rhythm? y / n")
         if Antwoord2 == "y":
             in_list.clear()
             in_list = list(input("Type bpm then type your desired notelengths with 1 being a quarternote, 0.5 being a eighth.").split(' '))
+            bpm = in_list.pop(0)
         if Antwoord2 == "n":
-            t = 0
-    else:
-        print("error")
+            quit()
+#            t = 0
+#    else:
+#        print("error")
