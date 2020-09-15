c84de9fc9ecf2b64d53cf29072e1a5ac099eb3f3
harm <65293494+harmneidig@users.noreply.github.com>
2020-09-15 15:34:19 +0200

    added quit and fixed some bugs

--- CSD2a/single_sample_player/src/playdelay.py
+++ CSD2a/single_sample_player/src/playdelay.py
@@ -18,12 +18,11 @@ while t==0:
             time.sleep(float(f)*(float(60/(int(bpm)))))
     if Antwoord == "n":
         Antwoord2 = input("do you want to change your rhythm? y / n")
+#       if you say "y" to changing the rhythm, it clears the list and
         if Antwoord2 == "y":
             in_list.clear()
             in_list = list(input("Type bpm then type your desired notelengths with 1 being a quarternote, 0.5 being a eighth.").split(' '))
             bpm = in_list.pop(0)
+#       If you say "n" twice then the code quits
         if Antwoord2 == "n":
             quit()
-#            t = 0
-#    else:
-#        print("error")
