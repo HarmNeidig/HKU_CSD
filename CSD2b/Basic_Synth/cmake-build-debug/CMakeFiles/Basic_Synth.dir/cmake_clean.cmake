c084a16cbf8b7933b860fb77e734742807e6d0db
harm <65293494+harmneidig@users.noreply.github.com>
2020-11-17 14:06:57 +0100

    Projects for HKU

---
+++ CSD2b/Basic_Synth/cmake-build-debug/CMakeFiles/Basic_Synth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Basic_Synth"
+  "Basic_Synth.pdb"
+  "CMakeFiles/Basic_Synth.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Basic_Synth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
