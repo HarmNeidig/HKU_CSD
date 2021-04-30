84ca4364e204afd25baffecb7a2114ae604deb55
harmneidig <harmneidig@gmail.com>
2021-04-30 17:52:06 +0200

    Working on the converting shell script

--- CSD2d/BASH/src/convert.sh
+++ CSD2d/BASH/src/convert.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 input_dir=~/desktop/HKU_CSD/CSD2d/BASH/audio_resources/input/
-
+output_dir=~/desktop/HKU_CSD/CSD2d/BASH/audio_resources/output/
+file_extention=_converted.wav
+sample_freq=24000
 count=0
 
 for f in ${input_dir}*.mp3;
 do
   count=$(($count+1))
-  file $f ;
+  filename= basename ${f%%.*}
+  target_file=${output_dir}${filename}${extention}
+  ffmpeg -i ${f} ${target_file}
 done;
 
 echo "Files in the Directory: $count"
