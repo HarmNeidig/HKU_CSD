7d5a86506a3e9719c9d7fcb47fc02545afc8a88b
harmneidig <harmneidig@gmail.com>
2021-07-21 22:47:19 +0200

    Got the pitch tracking working.

--- CSD2d/MIR_webapp/sketch.js
+++ CSD2d/MIR_webapp/sketch.js
@@ -1,8 +1,52 @@
+const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
+// loading model from github, thanks to the CodingTrain <3
+let mic;
+let pitch;
+let freq = 0;
+
 function setup(){
-	createCanvas(100,100);
-	console.log('ml5 version:', ml5.version);
+	audioContext = new AudioContext();
+ 	let cnv = createCanvas(windowWidth, windowHeight);
+ 	textAlign(CENTER);
+ 	mic = new p5.AudioIn();
+ 	mic.start(listening);
+}
+
+function listening() {
+  console.log('listening');
+  pitch = ml5.pitchDetection(
+    model_url,
+    audioContext,
+    mic.stream,
+    modelLoaded
+  );
 }
 
 function draw(){
-	background(200);
+ 	background(0);
+ 	fill(255);
+ 	textAlign(CENTER, CENTER);
+ 	textSize(32);
+ 	text(freq.toFixed(2), width / 2, height - 150);
+	fill(255);
+ 	textAlign(CENTER, CENTER);
+ 	textSize(32);
+	text("Hz", width / 2, height - 100);
+}
+
+function modelLoaded() {
+  console.log('model loaded');
+  pitch.getPitch(gotPitch);
+}
+
+function gotPitch(error, frequency) {
+  if (error) {
+    console.error(error);
+  } else {
+    //console.log(frequency);
+    if (frequency) {
+      freq = frequency;
+    }
+    pitch.getPitch(gotPitch);
+  }
 }
