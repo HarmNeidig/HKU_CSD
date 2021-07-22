64db3c5fae842c27284724fd83a9f73ab942743a
harmneidig <harmneidig@gmail.com>
2021-07-22 15:47:05 +0200

    Added a begin sequence for my browser game

--- CSD2d/MIR_webapp/sketch.js
+++ CSD2d/MIR_webapp/sketch.js
@@ -2,7 +2,10 @@ const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/
 // loading model from github, thanks to the CodingTrain <3
 let mic;
 let pitch;
+let beginFreq = 0;
 let freq = 0;
+let gameStarted = false;
+let displayText = "Press spacebar to start game, press q to quit";
 
 function setup(){
 	audioContext = new AudioContext();
@@ -24,14 +27,11 @@ function listening() {
 
 function draw(){
  	background(0);
- 	fill(255);
- 	textAlign(CENTER, CENTER);
- 	textSize(32);
- 	text(freq.toFixed(2), width / 2, height - 150);
 	fill(255);
- 	textAlign(CENTER, CENTER);
- 	textSize(32);
-	text("Hz", width / 2, height - 100);
+	textAlign(CENTER, CENTER);
+	textSize(32);
+	text(displayText, width / 2, height - 150);
+	begin();
 }
 
 function modelLoaded() {
@@ -43,10 +43,28 @@ function gotPitch(error, frequency) {
   if (error) {
     console.error(error);
   } else {
-    //console.log(frequency);
     if (frequency) {
       freq = frequency;
     }
     pitch.getPitch(gotPitch);
   }
+}
+
+function begin() {
+  if (keyCode === 81) {
+		gameStarted = false;
+  } else if (keyCode === 32){
+		setBeginFreq(freq);
+    gameStarted = true;
+		displayText = "";
+	}
+}
+
+function setBeginFreq(freq){
+	if (gameStarted == false){
+		beginFreq = freq;
+		console.log("Begin frequency is ", beginFreq);
+	} else if (gameStarted == true){	
+		return beginFreq;
+	}
 }
